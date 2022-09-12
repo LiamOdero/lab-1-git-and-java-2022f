269eedaedb120c3b37c715688515b83383f36702
LiamOdero <liam.odero1@gmail.com>
2022-09-12 17:48:22 -0400

    Added testPersonalMethod to IntroLabTest and oderolia to IntroLab

--- src/test/java/IntroLabTest.java
+++ src/test/java/IntroLabTest.java
@@ -25,6 +25,14 @@ public class IntroLabTest {
         assertEquals(expected, actual);
     }
 
+
+    @Test(timeout = 10)
+    public void testPersonalMethod()    {
+        String expected = "Salutations! My name is Liam Odero! I like dogs more than cats! message";
+        String actual = IntroLab.oderolia();
+        assertEquals(expected, actual);
+    }
+
     /* TODO: Write a test case for the method you wrote in IntroLab.
      *       If done properly, you should be able to run IntroLabTest and see
      *       the test results.
